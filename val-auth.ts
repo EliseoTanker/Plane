55455c42a0d3197b3fe85167a9c2a81d7c62d15a
Esileo <eliseotanker@gmail.com>
2022-07-07 02:06:39 -0400

    Removed trailing commas (formatter)

--- val-auth.ts
+++ val-auth.ts
@@ -2,21 +2,15 @@ import axios from "axios";
 import * as https from "https";
 import * as fs from "fs";
 import * as path from "path";
-const ciphers = [
-  "TLS_CHACHA20_POLY1305_SHA256",
-  "TLS_AES_128_GCM_SHA256",
-  "TLS_AES_256_GCM_SHA384",
-  "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256",
-];
+const ciphers = ["TLS_CHACHA20_POLY1305_SHA256", "TLS_AES_128_GCM_SHA256", "TLS_AES_256_GCM_SHA384", "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256"];
 export const agent = new https.Agent({
   ciphers: ciphers.join(":"),
   honorCipherOrder: true,
-  minVersion: "TLSv1.2",
+  minVersion: "TLSv1.2"
 });
 export async function credentialsAuth(username: string, password: string) {
   // Stolen from https://github.com/ev3nvy/valorant-reauth-script/blob/f79a5efd3ecd7757bafa7f63a1d9ca579bd1bc58/index.js :>
-  if (!username || !password)
-    return console.log("No valorant auth username or password");
+  if (!username || !password) return console.log("No valorant auth username or password");
 
   const parseUrl = (uri) => {
     let url = new URL(uri);
@@ -36,14 +30,13 @@ export async function credentialsAuth(username: string, password: string) {
         nonce: 1,
         redirect_uri: "https://playvalorant.com/opt_in",
         response_type: "token id_token",
-        scope: "account openid",
+        scope: "account openid"
       },
       {
         headers: {
-          "User-Agent":
-            "RiotClient/43.0.1.4195386.4190634 rso-auth (Windows; 10;;Professional, x64)",
+          "User-Agent": "RiotClient/43.0.1.4195386.4190634 rso-auth (Windows; 10;;Professional, x64)"
         },
-        httpsAgent: agent,
+        httpsAgent: agent
       }
     )
   ).headers["set-cookie"].find((elem) => /^asid/.test(elem));
@@ -52,15 +45,14 @@ export async function credentialsAuth(username: string, password: string) {
     {
       type: "auth",
       username: username,
-      password: password,
+      password: password
     },
     {
       headers: {
         Cookie: cookie,
-        "User-Agent":
-          "RiotClient/43.0.1.4195386.4190634 rso-auth (Windows; 10;;Professional, x64)",
+        "User-Agent": "RiotClient/43.0.1.4195386.4190634 rso-auth (Windows; 10;;Professional, x64)"
       },
-      httpsAgent: agent,
+      httpsAgent: agent
     }
   );
   access_tokens.data.error ? console.log(access_tokens.data.error) : console;
@@ -71,61 +63,50 @@ export async function credentialsAuth(username: string, password: string) {
       {},
       {
         headers: {
-          Authorization: `Bearer ${tokens.access_token}`,
-        },
+          Authorization: `Bearer ${tokens.access_token}`
+        }
       }
     )
   ).data.entitlements_token;
-  tokens.subject = JSON.parse(
-    Buffer.from(tokens.access_token.split(".")[1], "base64").toString()
-  ).sub;
+  console.log(tokens.access_token.split(".")[1]);
+  tokens.subject = JSON.parse(Buffer.from(tokens.access_token.split(".")[1], "base64").toString()).sub;
   return tokens;
 }
 
 export async function lockfileAuth() {
-  let version = (await axios.get("https://valorant-api.com/v1/version")).data
-    .data.riotClientVersion;
+  let version = (await axios.get("https://valorant-api.com/v1/version")).data.data.riotClientVersion;
   async function axiosGet(url: string, headers: any, entitlements: any) {
     if (headers === "JWT") {
       var res = await axios.get(url, {
         headers: {
           "X-Riot-Entitlements-JWT": entitlements.token,
           "X-Riot-ClientVersion": version,
-          "X-Riot-ClientPlatform":
-            "ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9",
-          Authorization: "Bearer " + entitlements.accessToken,
+          "X-Riot-ClientPlatform": "ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9",
+          Authorization: "Bearer " + entitlements.accessToken
         },
         httpsAgent: new https.Agent({
-          rejectUnauthorized: false,
-        }),
+          rejectUnauthorized: false
+        })
       });
     } else {
       var res = await axios.get(url, {
         headers: headers,
         httpsAgent: new https.Agent({
-          rejectUnauthorized: false,
-        }),
+          rejectUnauthorized: false
+        })
       });
     }
     return res.data;
   }
 
-  const contents = await fs.promises.readFile(
-    path.join(
-      process.env.LOCALAPPDATA!,
-      "Riot Games\\Riot Client\\Config\\lockfile"
-    ),
-    "utf8"
-  );
+  const contents = await fs.promises.readFile(path.join(process.env.LOCALAPPDATA!, "Riot Games\\Riot Client\\Config\\lockfile"), "utf8");
   let lD: any = {};
   [lD.name, lD.pid, lD.port, lD.password, lD.protocol] = contents.split(":");
   let entitlements = await axiosGet(
     `https://127.0.0.1:${lD.port}/entitlements/v1/token`,
     {
-      Authorization:
-        "Basic " +
-        Buffer.from(`riot:${lD.password}`, "utf8").toString("base64"),
-      "X-Riot-ClientVersion": version,
+      Authorization: "Basic " + Buffer.from(`riot:${lD.password}`, "utf8").toString("base64"),
+      "X-Riot-ClientVersion": version
     },
     null
   ).catch(function (err) {
